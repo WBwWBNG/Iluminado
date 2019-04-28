7afacc026b2e368efeb17257b21031a0bf23c5ef
WBwWBNG <45474587+WBwWBNG@users.noreply.github.com>
2019-04-27 22:56:46 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main () {
+    int i {0};
+    for (; i <= 20000; ++i)
+        std::cout << "SÓ ESTUDO E NENHUMA DIVERSÃO FAZ DO EDUARDO UM GAROTO MAÇANTE ";
+    return 0;
+}
